1cd5047b8065c369966b9f3673cd70f732fbf095
Joakim Kemeny <joakim@kemeny.se>
2014-01-25 23:12:11 +0100

    Added som testdata.

--- demo/demo-server/src/main/resources/import.sql
+++ demo/demo-server/src/main/resources/import.sql
@@ -1,8 +1,50 @@
 -- Insert some test patients.
-insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('121212-1212', 'Kalle', 'Kula', 'Storgatan 1', '12345', 'Göteborg', '031-123456', '0708-123456');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120101-4219', 'Albert', 'Örwall', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120102-3218', 'Anders', 'Asplund', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120103-1217', 'Björn', 'Beskow', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120204-5216', 'Christian', 'Hilmersson', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120205-6215', 'Hans', 'Thunberg', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120306-7214', 'Håkan', 'Dahl', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120307-1213', 'Joakim', 'Kemeny', 'Fabriksgatan 13', '41250', 'Göteborg', '031-482708', '0708-277486');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120408-8212', 'Johanna', 'Essén', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120409-9211', 'Jonas', 'Behmer', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120510-4210', 'Kajsa', 'Andersson', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120511-2219', 'Kristin', 'Luhr', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120612-1218', 'Magnus', 'Ekstrand', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120613-6217', 'Magnus', 'Larsson', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120714-7216', 'Mats', 'Ekhammar', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120715-9215', 'Ola', 'Deibitsch', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120716-8214', 'Peter', 'Larsson', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120817-5213', 'Peter', 'Merikan', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120818-7212', 'Pär', 'Wenåker', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120819-4211', 'Roger', 'Lindsjö', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120920-1210', 'Sedina', 'Oruc', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120921-3219', 'Torbjörn', 'Claesson', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120922-1218', 'Anders', 'Forssell', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('121023-2217', 'Malin', 'Västernäs', 'Fabriksgatan 13', '41250', 'Göteborg', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('121024-1216', 'Anders', 'Ferrari', 'Drottninggatan 55', '11121', 'Stockholm', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('121025-8215', 'Jacob', 'Tardell', 'Drottninggatan 55', '11121', 'Stockholm', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120126-1214', 'Khaled', 'Daham', 'Drottninggatan 55', '11121', 'Stockholm', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120327-6213', 'Oskar', 'Thunman', 'Drottninggatan 55', '11121', 'Stockholm', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120428-6212', 'Stefan', 'Gustafsson', 'Drottninggatan 55', '11121', 'Stockholm', '', '');
+insert into patients(civic_reg_nr, first_name, last_name, street_address, zip_code, city, phone, mobile) values ('120629-8211', 'Örjan', 'Lundberg', 'Drottninggatan 55', '11121', 'Stockholm', '', '');
 
 -- Insert some notes.
-insert into notes(created_time, fk_patient, type, text, doctor) values ('2014-01-14 10:00:00', 1, 'Note', 'Ont i höger tå', 'Anna Dahl');
+insert into notes(created_time, fk_patient, type, text, doctor) values ('2014-01-27 10:00:00', 7, 'Symptom', 'Har ont i höger tå.', 'Anna Dahl');
+insert into notes(created_time, fk_patient, type, text, doctor) values ('2014-01-27 10:05:00', 7, 'Assessment', 'Har förmodligen åkt skidor lite väl länge.', 'Anna Dahl');
+insert into notes(created_time, fk_patient, type, text, doctor) values ('2014-01-27 10:06:00', 7, 'Action', 'Har skickat remiss till operation för amputation.', 'Anna Dahl');
 
 -- Insert some appointments.
-insert into appointments(start_time, end_time, fk_patient, notes) value ('2014-01-15 10:00:00', '2014-01-15 12:00:00', 1, 'Var i tid');
+insert into appointments(start_time, end_time, notes) values ('2014-01-22 09:00:00', '2014-01-24 12:00:00', 'Kristin Luhr');
+insert into appointments(start_time, end_time, notes) values ('2014-01-23 08:00:00', '2014-01-23 10:00:00', 'Mats Ekhammar');
+insert into appointments(start_time, end_time, notes) values ('2014-01-24 13:00:00', '2014-01-24 15:00:00', 'Magnus Larsson');
+insert into appointments(start_time, end_time, notes) values ('2014-01-27 08:00:00', '2014-01-27 10:00:00', 'Joakim Kemeny');
+insert into appointments(start_time, end_time, notes) values ('2014-01-27 10:30:00', '2014-01-27 12:00:00', 'Örjan Lundberg');
+insert into appointments(start_time, end_time, notes) values ('2014-01-27 13:00:00', '2014-01-27 15:30:00', 'Malin Västernäs');
+insert into appointments(start_time, end_time, notes) values ('2014-01-28 09:00:00', '2014-01-28 11:00:00', 'Pär Wenåker');
+insert into appointments(start_time, end_time, notes) values ('2014-01-28 13:00:00', '2014-01-28 15:30:00', 'Roger Lindsjö');
+insert into appointments(start_time, end_time, notes) values ('2014-01-29 09:00:00', '2014-01-29 12:00:00', 'Tutorials');
+insert into appointments(start_time, end_time, notes) values ('2014-01-29 13:00:00', '2014-01-29 17:30:00', 'Cadec');
+insert into appointments(start_time, end_time, notes) values ('2014-01-29 17:30:00', '2014-01-29 23:30:00', 'After Cadec');
+insert into appointments(start_time, end_time, notes) values ('2014-01-30 07:30:00', '2014-01-30 09:30:00', 'Kajsa Andersson');
+insert into appointments(start_time, end_time, notes) values ('2014-01-30 10:00:00', '2014-01-30 12:00:00', 'Stefan Gustavsson');
